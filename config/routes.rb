f7bfee67856052340f55879bcc3c644a2b041c62
businesskanki <businesskanki@gmail.com>
2022-10-25 08:33:44 +0300

    hellow world

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'pages/hello'
+  get 'home/index'
+  root 'pages#hello'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
